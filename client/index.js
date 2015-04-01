0decf6be4d9fa460a55e2610be4484c422e4fc09
Ionică Bizău <bizauionica@yahoo.com>
2015-04-01 11:56:00 +0300

    JSDoc comments

--- client/index.js
+++ client/index.js
@@ -1,9 +1,27 @@
+// Dependencies
 var $ = require("/libs/jquery");
 
+/*!
+ * getForm
+ * Returns the form element.
+ *
+ * @name getForm
+ * @function
+ * @param {Event} ev The event object.
+ * @param {Object} data The data object.
+ * @return {jQuery} The jQuery form element.
+ */
 function getForm(ev, data) {
     return (ev && ev.target && $(ev.target)) || (data.sel && $(data.sel));
 }
 
+/*!
+ * init
+ *
+ * @name init
+ * @function
+ * @return {undefined}
+ */
 exports.init = function () {
     var self = this;
     var $container = $(self._config.form || "body");
@@ -14,6 +32,16 @@ exports.init = function () {
     });
 };
 
+/**
+ * fill
+ * Fills a form with data provided in `data.data`.
+ *
+ * @name fill
+ * @function
+ * @param {Event} ev The event object.
+ * @param {Object} data The data object.
+ * @return {undefined}
+ */
 exports.fill = function (ev, data) {
     var $elm = getForm(ev, data);
     if ($elm.length) {
@@ -22,6 +50,16 @@ exports.fill = function (ev, data) {
     $elm.trigger("serializer:fill", [data.data]);
 };
 
+/**
+ * submit
+ * Request a submit event from form serializer.
+ *
+ * @name submit
+ * @function
+ * @param {Event} ev The event object.
+ * @param {Object} data The data object.
+ * @return {undefined}
+ */
 exports.submit = function (ev, data) {
     var $elm = getForm(ev, data);
     if ($elm.length) {
